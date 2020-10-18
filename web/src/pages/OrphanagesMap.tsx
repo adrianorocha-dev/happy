d3e30c7af0c4d32b83868480141d0f3ae0d15415
Adriano Rocha <adriano.olr@gmail.com>
2020-10-17 23:05:51 -0300

    [Aula 3] - Versão web finalizada

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import api from '../services/api';
+
+import mapIcon from '../utils/mapIcon';
 
 import mapMarkerImg from '../assets/marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
 const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  console.log(orphanages);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -34,9 +54,29 @@ const OrphanagesMap: React.FC = () => {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        {orphanages.map(orphanage => (
+          <Marker
+            position={{ lat: orphanage.latitude, lng: orphanage.longitude }}
+            icon={mapIcon}
+            key={orphanage.id}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 const Routes: React.FC = () => {
   return (
@@ -10,6 +12,9 @@ const Routes: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/app" component={OrphanagesMap} />
+
+        <Route exact path="/orphanages/create" component={CreateOrphanage} />
+        <Route exact path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
